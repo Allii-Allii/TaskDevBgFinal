057eb2db7767c213e725c728d368adf36041ad26
Java <ao.alina@gmail.com>
2024-04-17 20:47:13 +0300

    //hббg

--- src/test/java/org/selenium/steps/TestSteps.java
+++ src/test/java/org/selenium/steps/TestSteps.java
@@ -15,25 +15,32 @@ public class TestSteps {
     public void theUserClickEnterButton() {
         loginPage.header().clickEnterButton();
     }
+
     @Given("the user is logged in with username {string} and password {string}")
-    public void theUserIsLoggedInWithUserNameAndPassword (String username, String password){
+    public void theUserIsLoggedInWithUserNameAndPassword(String username, String password) {
     }
+
     @Given("the user set username {string}")
     public void theUserSetUsername(String username) {
+
         loginPage.enterLogin(username);
     }
 
     @Given("the user set password {string}")
     public void theUserSetPassword(String password) {
+
         loginPage.enterPassword(password);
     }
+
     @When("the user click Login Button")
     public void theUserClickLoginButton() {
+
         loginPage.clickLoginButton();
     }
 
     @Then("get error massage is {string}")
     public void getErrorMassageIs(String messageError) {
+
         assertEquals(loginPage.getErrorMassage(), messageError);
     }
 
